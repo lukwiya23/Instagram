d5054cd9910d378b5a70a5f03ae44278d1fcc28a
LukwiyaBonnie <luk23bonnie8@gmail.com>
2019-09-13 23:40:12 +0300

    setting up instagram camera

--- lib/insta_home.dart
+++ lib/insta_home.dart
@@ -5,6 +5,7 @@ class InstaHome extends StatelessWidget {
     backgroundColor: Color(0xfff8faf8),
     centerTitle: true,
     elevation: 1.0,
+    leading: Icon(Icons.camera_alt),
   );
   @override
   Widget build(BuildContext context) {
