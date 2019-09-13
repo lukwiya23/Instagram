d17d0d86396f8b45faedb8ca1956100d9132aff9
LukwiyaBonnie <luk23bonnie8@gmail.com>
2019-09-13 23:21:47 +0300

    setting up

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,11 @@ import 'package:flutter/material.dart';
 void main()=> runApp(MyApp());
 
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
