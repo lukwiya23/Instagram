33c2a62477cac80646cfd5d833859551de78e5c8
LukwiyaBonnie <luk23bonnie8@gmail.com>
2019-09-13 23:38:39 +0300

    designing the appbar

--- lib/insta_home.dart
+++ lib/insta_home.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class InstaHome extends StatelessWidget {
+  final topBar = AppBar(
+    
+  );
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
       
     );
   }
