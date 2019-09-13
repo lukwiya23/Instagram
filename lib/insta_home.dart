009d0385955136f39c144938ce775d95a011d5f7
LukwiyaBonnie <luk23bonnie8@gmail.com>
2019-09-13 23:42:58 +0300

    added the send message button

--- lib/insta_home.dart
+++ lib/insta_home.dart
@@ -6,6 +6,16 @@ class InstaHome extends StatelessWidget {
     centerTitle: true,
     elevation: 1.0,
     leading: Icon(Icons.camera_alt),
+    title: SizedBox(
+      height: 35.0,
+      child: Image.asset("assets/images/insta_logo.png"),
+    ),
+    actions: <Widget>[
+      Padding(
+        padding: const EdgeInsets.only(right: 12.0),
+        child: Icon(Icons.send),
+      )
+    ],
   );
   @override
   Widget build(BuildContext context) {
