e757c7306e737c9ccebdbba2be59c011a3d4bd23
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-14 10:03:07 +0300

    creating the insta posts ui

--- lib/insta_posts.dart
+++ lib/insta_posts.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+
+class InstaPosts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
