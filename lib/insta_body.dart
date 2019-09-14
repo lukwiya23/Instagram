f1d7bc745bb9c17562102d71c679143cae2b40d0
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-14 09:58:29 +0300

    .

--- lib/insta_body.dart
+++ lib/insta_body.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class InstaBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+
+      ],
+    );
+  }
+}
