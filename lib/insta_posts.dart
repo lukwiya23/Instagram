0b10339f4b9334d0364aff897df52c07d96afdd5
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-14 10:05:52 +0300

    .

--- lib/insta_posts.dart
+++ lib/insta_posts.dart
@@ -1,10 +1,13 @@
+import 'package:first_app/insta_stories.dart';
 import 'package:flutter/material.dart';
 
 
 class InstaPosts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return ListView.builder(
+      itemCount: 6,
+      itemBuilder: (context, index)=>index=0?SizedBox(child: InstaStories(),),
       
     );
   }
