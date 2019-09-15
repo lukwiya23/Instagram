dfbceb0950fef265b78365b8d9239644184895d0
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-15 16:48:45 +0300

    .

--- lib/insta_posts.dart
+++ lib/insta_posts.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 class InstaPosts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var deviceSize = MediaQuery.of(context).size;
     return ListView.builder(
       itemCount: 6,
       itemBuilder: (context, index)=>index==0?SizedBox(child: InstaStories(),
