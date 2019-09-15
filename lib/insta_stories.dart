76f9953af12916d56469332fdd99d3e15ac09c68
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-15 13:56:56 +0300

    designing the stories panel

--- lib/insta_stories.dart
+++ lib/insta_stories.dart
@@ -4,7 +4,10 @@ class InstaStories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      margin: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+      ),
     );
   }
 }
